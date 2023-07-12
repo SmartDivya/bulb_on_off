3ea4fdf31f799b32d40e386594be9e82a0b4ee31
SmartDivya <117432328+SmartDivya@users.noreply.github.com>
2023-07-12 10:49:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+
+var on_btn = document.getElementById("on");
+var off_btn =document.getElementById("off");
+var img=document.getElementById("Bulb");
+
+function bulb_on(){
+img.src="./image 1/download (1).jpg";
+}
+
+function bulb_off(){
+img .src="./image 1/download (2).jpg";
+}
